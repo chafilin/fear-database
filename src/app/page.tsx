2d5800f2f553f1deb978bd17f55290c4142d4362
Chafilin <chafiliny@gmail.com>
2023-10-07 21:55:20 +0600

    add styles for index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ import RootLayout from "@/app/layout";
 export default function Home() {
   return (
     <div className={styles.root}>
-      <h1>БИПЯ</h1>
-      <h2>Бюро Исследований Паранормальных Явлений</h2>
+      <h1 className={styles.header}>
+        Бюро Исследований Паранормальных Явлений
+      </h1>
+      <h2 className={styles.subheader}>Миссия Джейлбрик</h2>
       <div className={styles.linklist}>
+        <Link href="/connection">Связь с командами</Link>
+        <Link href="/logs">Логи экспедиции</Link>
+        <Link href="/encyclopedia">О городе</Link>
+        <Link href="">Об оборудовании</Link>
         <Link href="/ghosts">Призраки</Link>
-        <Link href="/connection">Связь</Link>
-        <Link href="/logs">Логи</Link>
-        <Link href="/encyclopedia">Энциклопедия</Link>
+        <Link href="/ghosts/search">Определить призрака</Link>
       </div>
     </div>
   );

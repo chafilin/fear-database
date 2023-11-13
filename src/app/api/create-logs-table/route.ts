273496465d2d4163b648e195b62f2f625f154ad1
Chafilin <chafiliny@gmail.com>
2023-11-13 20:34:03 +0600

    add check is table exist before creation

--- src/app/api/create-logs-table/route.ts
+++ src/app/api/create-logs-table/route.ts
@@ -1,15 +1,29 @@
 import { sql } from "@vercel/postgres";
 
 export async function GET() {
-  await sql`
-    CREATE TABLE logs (
-      id VARCHAR(255) PRIMARY KEY,
-      message VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP NOT NULL
+  const result = await sql`
+    SELECT EXISTS (
+      SELECT FROM information_schema.tables
+      WHERE  table_schema = 'public'
+      AND    table_name   = 'logs'
     );
   `;
 
-  return new Response(JSON.stringify({ message: "ok" }), {
+  if (result.rows[0].exists) {
+    return new Response(JSON.stringify({ message: "table exists" }), {
+      headers: { "content-type": "application/json" },
+    });
+  }
+
+  await sql`
+      CREATE TABLE logs (
+        id VARCHAR(255) PRIMARY KEY,
+        message VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP NOT NULL
+      );
+    `;
+
+  return new Response(JSON.stringify({ message: "table created" }), {
     headers: { "content-type": "application/json" },
   });
 }

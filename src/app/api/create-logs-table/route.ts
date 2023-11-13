80786ad153ca0c0d4ecc162bc6d6ca6bc0e62ac4
Chafilin <chafiliny@gmail.com>
2023-11-13 20:29:58 +0600

    fix create-table return

--- src/app/api/create-logs-table/route.ts
+++ src/app/api/create-logs-table/route.ts
@@ -1,11 +1,15 @@
 import { sql } from "@vercel/postgres";
 
-export function GET() {
-  return sql`
+export async function GET() {
+  await sql`
     CREATE TABLE logs (
       id VARCHAR(255) PRIMARY KEY,
       message VARCHAR(255) NOT NULL,
       created_at TIMESTAMP NOT NULL
     );
   `;
+
+  return new Response(JSON.stringify({ message: "ok" }), {
+    headers: { "content-type": "application/json" },
+  });
 }

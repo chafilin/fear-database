8b9076ef046a66dd252c46e1504e86e8eda3bb9c
Schedrin Vladimir <chafiliny@gmail.com>
2024-05-21 04:37:19 +0600

    change viewport export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,11 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "Бюро Исследований Паранормальных Явлений",
   description: "Миссия Джейлбрик",
-  viewport: "width=device-width, initial-scale=1",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
